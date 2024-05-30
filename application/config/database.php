5e8897665d8b38e77315ced964f91f4a75e62287
ilyanwahid24 <144574841+ilyanwahid24@users.noreply.github.com>
2024-05-30 09:37:25 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '127.0.0.1',
 	'username' => 'pengolahan_citra_user',
 	'password' => 'SecureP455wd',
 	'database' => 'pengolahan_citra_db',
